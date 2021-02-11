d5fcc1a4fcd0acb38135788aee1399423eb071c6
paarhan <anparhan33@gmail.com>
2021-02-10 17:10:17 -1200

    tampilan 30%

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+//bottomnavscreen aces
+import 'package:hello_world_project/screens/bottom_nav_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+ return MaterialApp(
+      title: 'Flutter Covid-19 Dashboar UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:hello_world_project/config/palette.dart';
+import 'package:hello_world_project/widgets/widgets.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String _country = 'USA';
+  @override
+  Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(screenHeight),
+        ],
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildHeader(double screnHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          color: Pallette.primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40.0),
+            bottomRight: Radius.circular(40.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'COVID-19',
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                CountryDropdown(
+                  countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
+                  country: _country,
+                  onChanged: (val) => setState(() => _country = val),
+                ),
+              ],
+            ),
+            SizedBox(height: screnHeight * 0.03),
+            Column(
+              children: <Widget>[
+                Text('are you feeling sick?'),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
